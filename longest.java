283be71441042c750c4d4840c2e7d42e19303787
Soni R <93289594+Soni-20@users.noreply.github.com>
2022-04-07 16:13:52 +0530

    Add files via upload

---
+++ longest.java
@@ -0,0 +1,16 @@
+package WORK;
+import java.util.*;
+public class longest {
+
+	public static void main(String[] args) {
+		String n[]=new String[10];
+		int m1[]=new int[10];
+		Scanner in=new Scanner(System.in);
+		System.out.println("enter the text:");
+		m=in.nextLine();
+		
+		
+		
+	}
+
+}
